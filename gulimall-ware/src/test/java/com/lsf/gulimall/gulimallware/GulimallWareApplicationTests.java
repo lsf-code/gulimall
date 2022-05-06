1c0ebeac8ce68eb8f33c2e227a8840daa1345abe
lsf-code <254396080@qq.com>
2022-05-06 16:31:15 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/lsf/gulimall/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsf.gulimall.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
